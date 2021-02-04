a4795dab2bd8d60f9b97978fb1949cfb5d68e62a
Tobias Schraink <tobi.schraink@gmail.com>
2021-02-03 21:27:51 -0500

    added a read_gct function to parsers.py The function can read in both annotation and the data from a .gct file. It can also filter the sample columns and the metadata columns.

--- phosphodisco/parsers.py
+++ phosphodisco/parsers.py
@@ -35,6 +35,61 @@ def read_protein(file_path: str) -> DataFrame:
         ['na', 'NA', 'NAN', 'nan', 'NaN', 'Na'], np.nan
     ).astype(float)
 
+def read_gct(path: str, 
+             index_cols: list=['geneSymbol', 'variableSites'], 
+             regex: str=None, 
+             sample_cols: list=None, 
+             annotation_rows: list=None
+            ):
+    """
+    Reads in a gct file and formats the dataframe so it's ready for phospho disco
+    path: path to file.gct
+    index_cols:  columns which to use as an index. For phospho/acetyl, etc this should 
+                 be two columns e.g. ['geneSymbol', 'variableSites'] whereas for protein it's one column, e.g. ['geneSymbol']
+    regex:       [optional] regular expression to quickly subselect sample columns e.g.  
+                 to get only sample columns that end in a digit: r'.*\d$'
+    sample_cols: [optional] to select sample columns using exact names; not used if you provided a regex
+    
+    returns: pd.DataFrame with sample columns and index_cols
+    """
+    with open(path, 'r') as handle:
+        next(handle)
+        #the 2nd row of a gct file gives us the dimensions
+        nrows, ncols, nrowmeta, ncolsmeta = [int(i) for i in next(handle).split()] 
+    df = pd.read_csv(
+        path, sep='\t', skiprows=2, low_memory=False
+    ).replace(
+         ['na', 'NA', 'NAN', 'nan', 'NaN', 'Na'], np.nan
+    )
+    # the metadatatable is transposed in the gct file, hence we are indexing everything but 
+    # the first ncolsmeta rows, and everything but the first nrowsmeta columns
+    sample_df = df.set_index(index_cols).iloc[ncolsmeta:, nrowmeta-1:].copy()
+    annots_df = df.set_index(df.columns[0]).iloc[:ncolsmeta, nrowmeta-1:].copy()
+    if regex is not None:
+        sample_df = sample_df.loc[:,sample_df.columns.str.match(regex)]
+        annots_df = annots_df.loc[:,annots_df.columns.str.match(regex)]
+    elif sample_cols is not None:
+        try:
+            sample_df = sample_df.loc[:, sample_cols]
+            annots_df = annots_df.loc[:, sample_cols]
+        except KeyError:
+            non_matched_cols = set(sample_cols).difference(sample_df.columns)
+            raise IndexError(
+                f"The following columns were not found in the sample columns of the provided gct file \npath:\n{path}\
+                \nmismatched columns:\n{non_matched_cols}"
+            )
+    if annotation_rows is not None:
+        try:
+            annots_df = annots_df.loc[annotation_rows, :]
+        except KeyError:
+            non_matched_cols = set(annotation_rows).difference(annots_df.index)
+            raise IndexError(
+                f"The following columns were not found in the annotation rows columns of the provided gct file \npath:\
+                \n{path}\nmismatched columns:\n{non_matched_cols}"
+            )
+
+    return sample_df, annots_df
+
 
 def read_annotation(file_path: str) -> DataFrame:
     """Reads in sample annotation file. Sample as rows, annotations as columns.
